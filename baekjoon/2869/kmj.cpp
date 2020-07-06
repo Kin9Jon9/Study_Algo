ce2df3f908dd7e6f64998e4a775c25610c129c78
kin9jon9 <cvj9883@naver.com>
2020-07-07 02:14:27 +0900

    김민종 2869 추가

---
+++ baekjoon/2869/kmj.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	int A,B,V;
+	int result = 0;
+	cin >> A >> B >> V;
+	
+	result = ( (V - A) / (A - B) );
+	if ( (V - A) % (A - B) != 0) result++;
+	
+	cout << ++result << endl;
