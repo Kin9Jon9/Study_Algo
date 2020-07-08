e9ea83dae929820d264c182b1c78f7adda3b9893
haaseungwoo <hansi100@naver.com>
2020-07-08 09:02:39 +0900

    한승우 동지의 혁명적인 풀이 방법

--- baekjoon/2858/hsw.cpp
+++ baekjoon/2858/hsw.cpp
@@ -1,1 +1,15 @@
-비공개
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main(){
+  int L,W,R,B;
+  int sum, multi;
+  cin >> R >> B;
+  sum = (R + 4)/2;
+  multi = ( B - 4 ) + (2 * sum);
+  L = -(-sum + (sqrt(sum*sum-4*multi)))/2;;
+  W = -(-sum - (sqrt(sum*sum-4*multi)))/2;
+  cout << W << " " << L << endl;
+}
